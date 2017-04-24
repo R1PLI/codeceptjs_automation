6b6adc8cccd0d5f60bc52159fd16890575ce85d5
RIPLI <treadz11@gmail.com>
2017-04-24 22:46:42 +0300

    Delete redundant selenium.bat file, update gulpfile with 'selenium start' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 let gulp = require('gulp');
 let del = require('del');
+let exec = require('child_process').exec;
 
-gulp.task('clean:output_folder', function () {
+gulp.task('clean', function () {
     return del([
         'output/*',
     ]);
 });
 
-gulp.task('default', ['clean:output_folder']);
+gulp.task('selenium', function (cb) {
+    exec('selenium-standalone start', function (err, stdout, stderr) {
+        console.log("Selenium is running");
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('default', function () {
+    console.log('Hi, this is Gulp.js!');
+});
