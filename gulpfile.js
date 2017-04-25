d6500ef7460e322d289645dcd4352438888c2a88
RIPLI <treadz11@gmail.com>
2017-04-25 11:12:16 +0300

    Changes in package and gulpfile from work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 let gulp = require('gulp');
 let del = require('del');
+let exec = require('child_process').exec;
 
-gulp.task('clean:output_folder', function () {
+gulp.task('clean', function () {
     return del([
         'output/*',
     ]);
 });
 
-gulp.task('default', ['clean:output_folder']);
+gulp.task('server', function (cb) {
+		exec('selenium-standalone start', function (err, stdout, stderr) {
+				console.log(stdout);
+				console.log(stderr);
+				cb(err);
+		});
+});
+
+gulp.task('default', function () {
+		console.log('This is gulp, hi!');
+});
