a0c5103e344689baacde228fce55088341c5b857
RIPLI <treadz11@gmail.com>
2017-04-25 21:57:29 +0300

    Change test structure

--- tests/google_search_test.js
+++ tests/google_search_test.js
@@ -6,19 +6,28 @@ Feature('google search');
 
 Before((I) => {
     I.amOnPage('/');
+
+});
+
+Scenario('Test codeceptJS main page motto', (I) => {
     I.see('Gmail');
     I.fillField('q', 'codeceptJs');
     I.pressKey('Enter');
     I.click('CodeceptJS');
-});
-
-Scenario('Test codeceptJS main page motto', (I) => {
-    I.see("MODERN ERA ACCEPTANCE TESTING FOR NODEJS"); //without css selector
+    I.see("SCENARIO DRIVEN");
+    I.see("BACKEND AGNOSTIC");
+    I.see("INTERACTIVE SHELL");
     // I.see('MODERN ERA ACCEPTANCE TESTING FOR NODEJS', '.motto'); - with css selector
 });
 
 Scenario('Test codeceptJS main page motto using assert', function*(I) {
-    let text = yield I.grabTextFrom('.motto');
+    let text;
+
+    I.see('Gmail');
+    I.fillField('q', 'codeceptJs');
+    I.pressKey('Enter');
+    I.click('CodeceptJS');
+    text = yield I.grabTextFrom('.motto');
     assert.equal(text, 'MODERN ERA ACCEPTANCE TESTING FOR NODEJS');
 });
 
